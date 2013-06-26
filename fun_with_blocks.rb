66044abb96a9b9bb07557f11d214fefb59c277b1
Eric Slater <eric9970@sbcglobal.net>
2013-06-26 13:38:07 -0500

    added example where i re-implement each

--- fun_with_blocks.rb
+++ fun_with_blocks.rb
@@ -30,3 +30,33 @@ end
 func = i_return_a_block
 func.call
 
+#---------
+# My List
+#---------
+
+class MyList
+
+  def initialize
+    @list = []
+  end
+
+  def add(stuff)
+    @list << stuff
+  end
+
+  def each (&block)
+    for i in 0..@list.size do
+      block.call(@list[i])
+    end
+  end
+
+end
+
+my_list = MyList.new
+my_list.add("a")
+my_list.add("b")
+my_list.add("c")
+my_list.each{ |item|
+  puts item
+}
+
